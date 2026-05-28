36bfa903851f91328ae85159cb0bbf6d9343de6e
akshithakethe <akshithakethe@gmail.com>
2026-05-28 11:19:22 +0530

    fix render

--- app.py
+++ app.py
@@ -274,6 +274,8 @@ def download():
     response.headers["Content-Disposition"] = "attachment; filename=report.pdf"
 
     return response
+    import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
